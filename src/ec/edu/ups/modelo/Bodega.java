6a1154763ddee7b9b923e8809f9e3f05ce0f7eeb
linar <linar@DESKTOP-OVM3OQ8>
2020-07-22 17:13:01 -0500

    Crecion de atributos de la clase Bodega y Cliente.

--- src/ec/edu/ups/modelo/Bodega.java
+++ src/ec/edu/ups/modelo/Bodega.java
@@ -10,5 +10,78 @@ package ec.edu.ups.modelo;
  * @author HI andres
  */
 public class Bodega {
+    /**
+    * Atributos que seran utilizados en la gestion de la 
+    * informacion.
+    */
     
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private int numeroDeEmpleados;
+    private int muellesDeDesenbarque;
+    private int transportes;
+
+    // Constructor sin parametros.
+    public Bodega() {
+    }
+
+    // Constructor con parametros. 
+    public Bodega(String nombre, String direccion, String telefono, int numeroDeEmpleados, int muellesDeDesenbarque, int transportes) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.numeroDeEmpleados = numeroDeEmpleados;
+        this.muellesDeDesenbarque = muellesDeDesenbarque;
+        this.transportes = transportes;
+    }
+    
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getNumeroDeEmpleados() {
+        return numeroDeEmpleados;
+    }
+
+    public void setNumeroDeEmpleados(int numeroDeEmpleados) {
+        this.numeroDeEmpleados = numeroDeEmpleados;
+    }
+
+    public int getMuellesDeDesenbarque() {
+        return muellesDeDesenbarque;
+    }
+
+    public void setMuellesDeDesenbarque(int muellesDeDesenbarque) {
+        this.muellesDeDesenbarque = muellesDeDesenbarque;
+    }
+
+    public int getTransportes() {
+        return transportes;
+    }
+
+    public void setTransportes(int transportes) {
+        this.transportes = transportes;
+    }
 }
--- src/ec/edu/ups/modelo/Cliente.java
+++ src/ec/edu/ups/modelo/Cliente.java
@@ -5,10 +5,112 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.Objects;
+
 /**
  *
  * @author HI andres
  */
 public class Cliente {
+    /**
+    * Atributos que seran utilizados en la gestion de la 
+    * informacion.
+    */
     
+    private String NombreDelCliente;
+    private String RUC;
+    private String Direccion;
+    private String Telefono;
+    private String Correo;
+    private String CodigoDeCliente;
+    
+    // Constructor sin parametros.
+    public Cliente() {
+    }
+     
+    // Constructor con parametros.
+    public Cliente(String NombreDelCliente, String RUC, String Direccion, String Telefono, String Correo, String CodigoDeCliente) {
+        this.NombreDelCliente = NombreDelCliente;
+        this.RUC = RUC;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+        this.Correo = Correo;
+        this.CodigoDeCliente = CodigoDeCliente;
+    }
+    
+    // Getters y Setters
+    public String getNombreDelCliente() {
+        return NombreDelCliente;
+    }
+
+    public void setNombreDelCliente(String NombreDelCliente) {
+        this.NombreDelCliente = NombreDelCliente;
+    }
+
+    public String getRUC() {
+        return RUC;
+    }
+
+    public void setRUC(String RUC) {
+        this.RUC = RUC;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getCodigoDeCliente() {
+        return CodigoDeCliente;
+    }
+
+    public void setCodigoDeCliente(String CodigoDeCliente) {
+        this.CodigoDeCliente = CodigoDeCliente;
+    }
+    
+    // Equals Y HashCode
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.CodigoDeCliente);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.CodigoDeCliente, other.CodigoDeCliente)) {
+            return false;
+        }
+        return true;
+    } 
 }
